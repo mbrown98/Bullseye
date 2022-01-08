c865c1e9cb8b74281196c9551bc68f099372941a
Matthew Brown <mbrown@geomarvel.com>
2022-01-07 19:59:16 -0500

    first commit

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                Text("89")
+            }
+            HStack {
+                Text("0")
+                Slider(value: .constant(50), in: 1.0...100)
+                Text("100")
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Hit me")
+            }
+        }
+         
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
         ContentView()
+       
     }
 }
