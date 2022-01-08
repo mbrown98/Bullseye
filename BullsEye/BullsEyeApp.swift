7681d9399a65a28968670de2b1736891a9550ed6
Matthew Brown <mbrown@geomarvel.com>
2022-01-07 19:47:14 -0500

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Matt Brown on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
